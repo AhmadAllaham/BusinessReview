cf3613f51561d3c2a6a70973a3c739e767a49b29
AhmadAllaham <ahmad22allaham@gmail.com>
2026-07-29 12:30:32 +0300

    Add Firebase login, country permissions, and admin data upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// Firebase web configuration is public client configuration, not a password.
+window.BR_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDM9ruoCNB9AtrRYd16rJXC-KagUDLjKkA",
+  authDomain: "businessreview-777a3.firebaseapp.com",
+  projectId: "businessreview-777a3",
+  storageBucket: "businessreview-777a3.firebasestorage.app",
+  messagingSenderId: "608491630593",
+  appId: "1:608491630593:web:915a4ac7c0c6b35a9ad000"
+};
